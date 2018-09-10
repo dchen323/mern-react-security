96d5ec9c71e76ce2ad0f9ded1d3b013625d0ec9c
Daniel <d.chen323@gmail.com>
2018-09-10 16:21:58 -0700

    Test that component escapes user input tags

--- security_examples/components/EscapeInput.js
+++ security_examples/components/EscapeInput.js
@@ -3,11 +3,25 @@ import React, { Component } from "react";
 const inputString = "<input type='text' defaultValue='rendered'/>";
 
 export default class EscapeInput extends Component {
+  constructor() {
+    super();
+    this.state = { title: "" };
+    this.updateTitle = this.updateTitle.bind(this);
+  }
+
+  updateTitle(e) {
+    this.setState({ title: e.target.value });
+  }
+
   render() {
     return (
       <div>
         <h3>Safe React Input</h3>
         <content>{inputString}</content>
+        <h4>{this.state.title}</h4>
+        <br />
+        <input placeholder="Set title.." onChange={this.updateTitle} />
+        <br />
       </div>
     );
   }
