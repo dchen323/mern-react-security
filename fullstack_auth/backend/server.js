cc17b0fe735b2bdcaf9221e65e1250b758571db4
Daniel <d.chen323@gmail.com>
2018-09-18 16:26:20 -0700

    Add username and password hashing to db entries

--- fullstack_auth/backend/server.js
+++ fullstack_auth/backend/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const SHA256 = require("crypto-js/sha256");
 
 const pool = require("./db");
+const { APP_SECRET } = require("./secret");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+const hash = str => {
+  return SHA256(`${APP_SECRET}${str}${APP_SECRET}`).toString();
+};
 
 /* Moved functionality to bash script
 const port = 3000;
@@ -24,20 +30,21 @@ app.get("/user/all", (req, res, next) => {
 
 app.post("/user/new", (req, res, next) => {
   const { username, password } = req.body;
+  const usernameHash = hash(username);
 
   pool.query(
-    "SELECT * FROM users WHERE username =$1",
-    [username],
+    "SELECT * FROM users WHERE username_hash =$1",
+    [usernameHash],
     (err, data) => {
       if (err) return next(err);
 
       if (data.rows.length === 0) {
         //insert a new user
         pool.query(
-          "INSERT INTO users(username, password) VALUES($1, $2)",
-          [username, password],
-          (q_err, q_res) => {
-            if (q_err) return next(q_err);
+          "INSERT INTO users(username_hash, password_hash) VALUES($1, $2)",
+          [usernameHash, hash(password)],
+          (qErr, qRes) => {
+            if (qErr) return next(qErr);
 
             res.status(200).json({ message: "Successfully created user!" });
           }
