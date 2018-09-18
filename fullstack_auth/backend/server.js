0d1fb8a5adc7df98f135ddc72f8a7b9c6c0ea5a0
Daniel <d.chen323@gmail.com>
2018-09-17 17:01:31 -0700

    Setup express

---
+++ fullstack_auth/backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+//Moved functionality to bash script
+// const port = 3000;
+
+// app.listen(port, () => console.log(`Listening on port: ${port}`));
+
+module.exports = app;
